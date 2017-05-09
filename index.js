cdd7cf74ccfbacb84314cc09dab83215c50b08a5
TechShiva <shaileng86@gmail.com>
2017-05-09 17:42:17 +0530

    restify replaceses with express

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });     
   ***********************************/
+
+  /*************************************
 var restify = require("restify");
 var msgBuilder = require("botbuilder");
 var callBuilder = require("botbuilder-calling");
@@ -35,8 +37,23 @@ function send(req, res, next) {
 }
 
 server.get("/", send);
+******************************************/
 
-   /*                      
+var express = require('express');
+var msgBuilder = require("botbuilder");
+var callBuilder = require("botbuilder-calling");
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));                      
+
+app.get('/', function(request, response) {
+  response.end("Welcome to NodeJS Bot");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});     
+
 var msgConnector = new msgBuilder.ChatConnector({
     appId: "f126e5af-beaa-4774-8144-4d2e300df102",
     appPassword: "Phm137R0bCG9vu5rXgCc0pw"
@@ -50,8 +67,10 @@ var callConnector = new callBuilder.CallConnector({
 });
 var callBot = new callBuilder.UniversalCallBot(callConnector);
 
-server.post("/api/messages", msgConnector.listen());
-server.post("/api/calls", callConnector.listen());
+//server.post("/api/messages", msgConnector.listen());
+//server.post("/api/calls", callConnector.listen());
+app.post("/api/messages",msgConnector.listen());
+app.post("/api/calls",callConnector.listen());
 
 msgBot.dialog("/", function (session) {
     session.send("hello Shailendra");
@@ -60,6 +79,6 @@ msgBot.dialog("/", function (session) {
 callBot.dialog("/", function (session) {
     session.send("Hello Shailendra. How may I help you");
 });
-     */
+
 
 
